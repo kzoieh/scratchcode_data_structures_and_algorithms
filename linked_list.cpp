e61d1ec456c162be078b32b5e68d92bd70f6e9c0
Kasturi <kasturi.4220@gmail.com>
2023-08-13 13:23:56 +0530

    linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,15 +1,30 @@
 #include<iostream>
 using namespace std;
+//-----------------------------------------------------------------------------//
+
+//LINKED LIST STRUCTURE
 
 struct Node
 {
     int data;
     Node *next;
 
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+
     Node(int value)
     {
         data = value;
         next = NULL;
+    }
+
+    Node(int value, Node *nextNode)
+    {
+        data = value;
+        next = nextNode;
     }
 
 };
@@ -28,7 +43,14 @@ struct Node
 
 // };
 
-//An explicitly defined functon to print values of Linked List
+
+//-----------------------------------------------------------------------------//
+
+
+//Explicitly defined functons to print values of Linked List//
+
+
+//Iterative
 void printList(Node* node)
 {
     while(node != NULL)
@@ -39,8 +61,23 @@ void printList(Node* node)
     return;
 }
 
-//An explicitly defined functon to add Node in front of Linked List
 
+//Recursive
+void printListRecursive(Node* node)
+{
+    if(node == NULL) return;
+    cout<<node->data;
+    printListRecursive(node->next);
+}
+
+
+//-----------------------------------------------------------------------------//
+
+
+//An explicitly defined functons to add Node in Linked List//
+
+
+//In Front of Linked List
 Node *addNode_atFront(Node *head, int value)
 {
     Node * newNode = new Node(value);
@@ -49,26 +86,25 @@ Node *addNode_atFront(Node *head, int value)
     return head;
 }
 
-//An explicitly defined functon to add Node in middle of Linked List
 
+//In Middle of Linked List
 void addNode_inMiddle(Node *node, int key, int value)
 {
     while(node->data != key)
     {
         node = node->next;
     }
-    
-    Node *temp = node->next;
+
     Node *newNode = new Node(value);
+    newNode->next = node->next;
     node->next = newNode;
-    newNode->next = temp;
 }
 
-//An explicitly defined functon to add Node at last of Linked List
 
+//At End of Linked List
 void addNode_atLast(Node *node, int value)
 {
-    while(node->data != NULL)
+    while(node->next != NULL)
     {
         node = node->next;
     }
@@ -77,6 +113,52 @@ void addNode_atLast(Node *node, int value)
     node->next = newNode;
     newNode->next = NULL;    
 }
+
+
+//-----------------------------------------------------------------------------//
+
+
+//An explicitly defined functon to delete Node in Linked List//
+
+//by value
+Node *deleteNode_byValue(Node *head, int val)
+{
+    if(head->data == val)
+    {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+
+    Node *temp = head;
+    Node *prev = NULL;
+
+    while(temp->data != val)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    prev->next = temp->next;
+    delete temp;
+    return head;
+}
+
+//by node
+void deleteNode_byNode(Node *node)
+{
+    Node* temp = node;
+    temp = temp->next;
+    node->data = node->next->data;
+    node->next = node->next->next;
+    delete temp;
+}
+
+
+//-----------------------------------------------------------------------------//
+
+
 
 int main()
 {
@@ -99,4 +181,33 @@ int main()
     addNode_atLast(head, 3);
     printList(head);
     cout<<endl;
+
+    addNode_atLast(head, 4);
+    printList(head);
+    cout<<endl;
+
+    addNode_atLast(head, 4);
+    printList(head);
+    cout<<endl;
+
+    addNode_atLast(head, 5);
+    printList(head);
+    cout<<endl;
+
+    addNode_atLast(head, 6);
+    printList(head);
+    cout<<endl;
+
+
+    head = deleteNode_byValue(head, 4);
+    head = deleteNode_byValue(head, 0);
+    head = deleteNode_byValue(head, 6);
+    printList(head);
+    cout<<endl;
+
+    deleteNode_byNode(head->next->next);
+    deleteNode_byNode(head);
+    printList(head);
+    //cout<<endl;
+
 }
